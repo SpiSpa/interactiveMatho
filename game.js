d9804f0995151ec2e15d57feb6b47d7d8e971b16
Sara Bellus <sarabellus@gmail.com>
2025-01-27 15:01:25 -0600

    Add files via upload

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-function pressAnswer 
+
+
+function answerPressed(clickedImage){
+    if ($("#" + clickedImage).hasClass("pressed")) {
+        $("#" + clickedImage).removeClass("pressed")
+    }
+    else {
+    $("#" + clickedImage).addClass("pressed");
+    }
+}
+
+$("img").click(function() {
+    var clickedImage = this.id;
+    console.log("I clicked " + clickedImage);
+    answerPressed(clickedImage);
+})
